1e84e96254d8c390a944d48039447892de5954c5
bx549 <bx549@protonmail.com>
2020-06-29 11:57:03 -0500

    add exercises to chapter 4 (with data files).

--- src/data-analysis.R
+++ src/data-analysis.R
@@ -1,11 +1,60 @@
 ## scratch file for chapter on data analysis
 
-Speed <- read.csv("../data/speed_gender_height.csv")
+## lognormal distribution parameter estimation
+Lifetimes <- read.table("../data/component-lifetimes.txt", header=T)
+
+(mu <- mean(log(Lifetimes$t)))
+(sigma <- sd(log(Lifetimes$t)))
+
+## mean time to failure
+(mttf <- exp(mu + (sigma^2)/2))
+
+## probability that a component lasts longer than 10,000 hours
+(p <- 1 - pnorm((log(10000)-mu)/sigma))
+
+## histogram and fitted distribution
+## put in section 4.2 graphics?
+
+
+## college students and driving speed
+Speed <- read.csv("../data/speed-gender-height.csv")
 ggplot(Speed) + geom_boxplot(aes(x=gender, y=speed), na.rm=TRUE)
 
 ggplot(Speed) + geom_point(aes(x=height, y=speed, color=gender), na.rm=TRUE)
 
 fm <- lm(speed ~ height + gender, data = Speed)
 summary(fm)
+
+
+## fruit spoilage
+Spoil <- read.table("../data/fruit-spoilage.txt", header=T)
+summary(Spoil)
+
+sd(Spoil$Number.of.Spoiled.Peaches)
+
+## an alternative way to get the quartiles
+quantile(Spoil$Number.of.Spoiled.Peaches, probs=c(.25,.50,.75))
+
+table(Spoil) # shows that the mode is 1
+
+## restaurant service times
+t <- rnorm(220, mean=70, sd=18)  # generate our own data
+idx <- sample(1:length(t), 50)   # choose a few times and make them longer
+t[idx] <- t[idx] + rexp(50, rate=1/12)
+hist(t)
+t <- round(t)
+write.table(t, file="../data/restaurant-service-times.txt", row.names=FALSE)
+
+Service <- read.table("../data/restaurant-service-times.txt", header=T)
+
+alpha <- 0.05 # the confidence level
+n <- nrow(Service) 
+xbar <- mean(Service$t)
+se <- sd(Service$t)/sqrt(n)
+cp <- qnorm(1 - alpha/2) # critical point using std normal distribution
+
+xbar + c(-1,1) * cp * se # a 95% confidence interval
+
+
 
 
